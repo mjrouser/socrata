91a31f481ffffa8a97d6d2f81f6e2013c291d1bc
mjrouser <matthew@mjrouser.com>
2014-09-04 21:52:55 -0400

    update

--- src/chart2.js
+++ src/chart2.js
@@ -6,16 +6,15 @@ d3.json('outputArray.json', function(data) {
         .color(d3.scale.category10().range())
         .margin({top: 30, right: 20, bottom: 50, left: 175})
         .color(d3.scale.category10().range())
-        //.showValues(true)           //Show bar value next to each bar.
-        //.tooltips(true)             //Show tooltips on hover.
-        //.transitionDuration(350)
+        .transitionDuration(350)
         .useInteractiveGuideline(true)
-        //.showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
+        
 
     chart.xAxis
-        //.tickValues([1960,1961,1962,1963,1964,1965,1966,1967,1968,1969,1970,1971,1972,1973,1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004])
+        .tickValues([-294187180,-262651180,-231115180,-199579180,-167956780,-136420780,-104884780,-73348780,-41726380,-10190380,21345620,52881620,84504020,116040020,147576020,179112020,210734420,242270420,273806420,305342420,336964820,368500820,400036820,431572820,463195220,494731220,526267220,557803220,589425620,620961620,652497620,684033620,715656020,747192020,778728020,810264020,841886420,873422420,904958420,936494420,1409947200,1409947260,1409947320,1409947380,1409947440])
+        
         .tickFormat(function(d){ 
-            return d3.time.format('%x')(new Date(d))
+            return d3.time.format('%y')(new Date(d))
         });
         
 
