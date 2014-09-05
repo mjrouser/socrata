4cd8f0e16f4c12468da34a6216b4809cffff8911
mjrouser <matthew@mjrouser.com>
2014-09-04 23:24:55 -0400

    little more

--- src/chart.js
+++ src/chart.js
@@ -1,23 +1,21 @@
 /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
 d3.json('outputArray.json', function(data) {
 nv.addGraph(function() {
-  var chart = nv.models.multiBarHorizontalChart()
-                .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
-                .transitionDuration(350)  //how fast do you want the lines to transition?
-                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                .showYAxis(true)        //Show the y-axis
-                .showXAxis(true)        //Show the x-axis
-  ;
+  var chart = nv.models.lineChart()
+                .x(function(d) { return d[0] })
+                .y(function(d) { return d[1] * 100 })
+                //.useInteractiveGuideline(true);
+
 
   chart.xAxis     //Chart x-axis settings
       .axisLabel('Year')
       .rotateLabels(-45)
-      .tickFormat(function(d) { return d3.time.format('%b %d')(new Date(d)); });
+      .tickFormat(function(d) { return d3.time.format('%y')(new Date(d)); 
+    });
 
   chart.yAxis     //Chart y-axis settings
       .axisLabel('Output')
-      .tickFormat('d');
+      .tickFormat(d3.format(',.003f'));
 
   /* Done setting the chart up? Time to render it!*/
   
--- src/chart2.js
+++ src/chart2.js
@@ -2,27 +2,31 @@ d3.json('outputArray.json', function(data) {
   nv.addGraph(function() {
     var chart = nv.models.cumulativeLineChart()
         .x(function(d) { return d[0] })
-        .y(function(d) { return d[1] })
+        .y(function(d) { return d[1] * 100 })
         .color(d3.scale.category10().range())
         .margin({top: 30, right: 20, bottom: 50, left: 175})
         .color(d3.scale.category10().range())
-        .transitionDuration(350)
+        //.transitionDuration(350)
         .useInteractiveGuideline(true)
         
 
     chart.xAxis
         .tickValues([-294187180,-262651180,-231115180,-199579180,-167956780,-136420780,-104884780,-73348780,-41726380,-10190380,21345620,52881620,84504020,116040020,147576020,179112020,210734420,242270420,273806420,305342420,336964820,368500820,400036820,431572820,463195220,494731220,526267220,557803220,589425620,620961620,652497620,684033620,715656020,747192020,778728020,810264020,841886420,873422420,904958420,936494420,1409947200,1409947260,1409947320,1409947380,1409947440])
-        
-        .tickFormat(function(d){ 
-            return d3.time.format('%y')(new Date(d))
+        .rotateLabels(-45)
+        .axisLabel('Year')
+        .tickFormat(function(d) { 
+            return d3.time.format('%Y')(new Date(d))
         });
+        
         
 
     chart.yAxis
-        .tickFormat(d3.format());
+        .axisLabel('Output')
+        .tickFormat(d3.format('+.003r'));
 
     d3.select('#chart svg')
         .datum(data)
+        .transition().duration(500)
         .call(chart);
 
     nv.utils.windowResize(chart.update);
