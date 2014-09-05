9d9cb4c0519e5f64f82a3495803f1c9cd9a565ac
mjrouser <matthew@mjrouser.com>
2014-09-04 20:50:12 -0400

    progress

--- src/chart2.js
+++ src/chart2.js
@@ -1,10 +1,11 @@
-d3.json('relativeOutputArray2.json', function(data) {
+d3.json('outputArray.json', function(data) {
   nv.addGraph(function() {
     var chart = nv.models.cumulativeLineChart()
         .x(function(d) { return d[0] })
         .y(function(d) { return d[1] })
         .color(d3.scale.category10().range())
-        //.margin({top: 30, right: 20, bottom: 50, left: 175})
+        .margin({top: 30, right: 20, bottom: 50, left: 175})
+        .color(d3.scale.category10().range())
         //.showValues(true)           //Show bar value next to each bar.
         //.tooltips(true)             //Show tooltips on hover.
         //.transitionDuration(350)
